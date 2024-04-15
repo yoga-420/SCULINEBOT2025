fdd85e080baf2b4363311cdc2d867e0b0b0e0512
w831152001 <w831152001@gmail.com>
2024-04-15 13:16:28 +0800

    try to fix

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ HF_TOKEN = os.environ.get('HF_TOKEN')
 headers = {"Authorization": f"Bearer {HF_TOKEN}"}
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
 def query(payload):
-    response = requests.post(API_URL, headers=headers, json=json.dumps(payload))
-    return response.json()
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return json.loads(response.text)
 
 app = Flask(__name__)
 
